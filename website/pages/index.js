f14967a3f478e0224ad426b9669ad4066104d8ef
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-11-30 12:34:34 +0530

    :recycle: added meta for website

--- website/pages/index.js
+++ website/pages/index.js
@@ -5,10 +5,13 @@ import Info_Follow from '../components/Info_Follow';
 import Info_Enjoy from '../components/Info_Enjoy';
 import Hero from '../components/Hero';
 
-
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Vibez</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Hero />
       <Info_Login />
       <Info_Setup />
