ad2db82aec3e4f0a4b173c5bef4bbc1046c3b7cc
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-12-01 19:42:28 +0530

    changed api url to deployed url

--- client/src/helper/sendApiRequest.js
+++ client/src/helper/sendApiRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const token = localStorage.getItem('jwt');
 
-axios.defaults.baseURL = '/api/v1/';
+axios.defaults.baseURL = 'https://server-vibez.herokuapp.com/api/v1/';
 if (token) {
   axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
 }
